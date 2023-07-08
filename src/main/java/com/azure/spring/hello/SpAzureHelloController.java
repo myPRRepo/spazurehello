cd76ce722043a0100f5a103cab49c6f1eee552eb
poonamgupta <poonamgupta@localhost>
2023-07-08 16:09:36 -0400

    third

--- src/main/java/com/azure/spring/hello/SpAzureHelloController.java
+++ src/main/java/com/azure/spring/hello/SpAzureHelloController.java
@@ -8,6 +8,6 @@ public class SpAzureHelloController {
 
 	 @GetMapping("/helloazure")
 	 public String sayHello(){
-		 return "My name is Azure spring";
+		 return "My name is Azure spring ";
 	 }
 }
